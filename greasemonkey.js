45b74cf7d99cad9e5c5165f42288752b06a8b273
flederwiesel <flederwiesel@fra-flugplan.de>
2022-12-26 02:15:30 +0100

    Add "computerbase.de".

--- greasemonkey.js
+++ greasemonkey.js
@@ -21,6 +21,8 @@ var selectors = [
   "#ccc,#ccc-module,#ccc-notify,#ccc-overlay",
   "#dpr-manager",
   "#ez-cookie-dialog-wrapper",
+  "#js-stats",
+  "#onetrust-consent-sdk",
   "#privacy-banner",
   "#tracking_leaderboard_container",
   // scripts and pixels
@@ -28,9 +30,12 @@ var selectors = [
   "*[src*=monetization]",
   "*[src*=ad-delivery],*[src*=doubleclick]",
   // cookie banners and trackers
+  "dialog.consent,dialog.js-consent",
+  "div.adbox,div.adbox-skyscraper,div.adbox-skyscraper2",
   "div.ezmob-footer",
   "div.fc-consent-root,div.fc-dialog-overlay",
   "div[class*=sc-15dps6c-0]",
+  ".stats-pixel",
   "*[title='Consent Management']",
   "iframe[name$=apiLocator],iframe[name^=googlefc]",
   "iframe[name|=goog_topics_frame]",
@@ -40,6 +45,14 @@ var selectors = [
 ];
 
 var iframes = [
+  "acdn.adnxs.com",
+  "ads.pubmatic.com",
+  "cdn.connectad.io",
+  "eus.rubiconproject.com",
+  "js-sec.indexww.com",
+  "onetag-sys.com",
+  "prebid.a-mo.net",
+  "u.openx.net",
 ];
 
 // This should be a "all scripts loaded" hook, however, I did not find one yet...
